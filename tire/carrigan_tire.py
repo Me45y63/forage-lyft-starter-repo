84d12803129dbf3daa921aad758cf76c13b44538
Ike <dylanraspino264@gmail.com>
2023-10-06 19:15:53 +0000

    clean code

--- tire/carrigan_tire.py
+++ tire/carrigan_tire.py
@@ -3,16 +3,9 @@ from .tire import Tire
 
 class CarriganTire(Tire):
     def __init__(self, worn_out):
-        self.worn_out = worn_out
-
-    def searchArray(self):
+        self.worn_out = worn_out       
+            
+    def tire_should_be_serviced(self):
         for i in self.worn_out:
             if i >= 0.9:
                 return True
-            
-
-    def tire_should_be_serviced(self):
-        if self.searchArray():
-            return True
-        else:
-            return False
--- tire/octoprime_tire.py
+++ tire/octoprime_tire.py
@@ -5,14 +5,5 @@ class OctoprimeTire(Tire):
     def __init__(self, worn_out):
         self.worn_out = worn_out
 
-    def calcTotal(self) -> int:
-        result = 0
-        for i in self.worn_out:
-            result += i
-        return result
-
     def tire_should_be_serviced(self):
-        if self.calcTotal() >= 3:
-            return True
-        else:
-            return False
+        return sum(self.worn_out) >= 3.0

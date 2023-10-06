041c2f2aad8dd51ff4e92a3898df0f3c7cfb2b65
Ike <dylanraspino264@gmail.com>
2023-10-06 19:08:00 +0000

    tests passed

--- tire/carrigan_tire.py
+++ tire/carrigan_tire.py
@@ -6,10 +6,10 @@ class CarriganTire(Tire):
         self.worn_out = worn_out
 
     def searchArray(self):
-        if 0.9 in self.worn_out:
-            return True
-        else:
-            return False
+        for i in self.worn_out:
+            if i >= 0.9:
+                return True
+            
 
     def tire_should_be_serviced(self):
         if self.searchArray():
